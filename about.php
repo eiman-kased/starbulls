149381453f312b693fcd4ce40004e0abf5d68693
Eiman Kased <eiman.kased@gmail.com>
2021-02-17 17:32:42 -0500

    renamed files and added the changed names in the href references

---
+++ about.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/willStarbull.css" />
+
+    <title>About Us</title>
+</head>
+
+<body>
+    <div id="socialMedia">
+        <a href="http://www.instagram.com/" class="btn btn-default" target="_blank">
+            <img src="images\instagram.png" alt="Instagram"></a>
+        <a href="http://www.facebook.com/" class="btn btn-default" target="_blank">
+            <img src="images\fb.png" alt="facebook"></a>
+        <a href="http://www.twitter.com/" class="btn btn-default" target="_blank">
+            <img src="images\_twitter.png" alt="twitter"></a>
+    </div>
+    <div class="header">
+        <div class="logo">
+            <a href="home.php" class="logo">
+                <img src="images/starbullslogo.png" alt="logo" />
+            </a>
+        </div>
+
+        <h1>STARBULLS
+            <div class="topnav">
+                <a href="menu.php">Menu</a>
+                <a href="specialMenu.php">Specials Of The Week</a>
+                <a href="about.php">About Us</a>
+                <a href="apply.php">Apply Now</a>
+            </div>
+        </h1>
+    </div>
+    </div>
+    <h2 class="us">About Us</h2>
+
+    <div class="company">
+        <h3>Company Information</h3>
+        <p>We are the StarBulls. The mighty, mighty StarBulls. Everywhere we go, people want to know:
+            WHO WE
+            ARE! So we tell
+            them: WE ARE THE STARBULLS; THE MIGHTY, MIGHTY STARBULLS!</p>
+    </div>
+
+    <div class="heritage">
+        <h3>Our Heritage</h3>
+        <p>We are humbled by the success of our company thus far. The name Starbulls was created by one of the founders:
+            Will. He came up with this insane concept of a restaurant that serves wings and coffee.
+            <p>
+                <p>How do we measure success? Simply: we started from the bottom, now we are here! Seriously,
+                    look at our site! A beauty to say the least.</p>
+                <p>Just a week ago, StarBulls did not exist. It has taken hours of dedication to get this
+                    company up
+                    on the server. Hours of frustration, trying to figure out why the code looks different on
+                    our
+                    screens. Hours of trial and error as we figured out where we went wrong. One thing the
+                    founders
+                    of StarBulls has known from the beginning is: Quitters Never Win. And Winners Never Quit. At
+                    StarBulls we are
+                    Winners.
+                </p>
+
+    </div>
+
+    <div class="tools">
+        <h3>Tools We Used</h3>
+        <p>Like any other company, the founders of StarBulls relied on various resources and tools that helped
+            them
+            get the site up and running! We would like to share these resources/tools with the
+            world.
+        </p>
+        <h3>They are:</h3>
+        <ol>
+            <li>Bill C.: You may have heard of him. The Man. The Myth. The Legend. If you haven't, then
+                maybe StarBulls is not for you!</li>
+            <li>Google.com: This one can have you running in circles BUT a lot of questions have been
+                answered by our friend Google.</li>
+            <li>Last but not least, the founders at StarBulls, relied on one another and other
+                classmates.
+            </li>
+        </ol>
+    </div>
+
+    <div class="cwing">
+
+    </div>
+
+    <div id="end">
+        <b>
+            Starbulls Wings and Coffee&nbsp; |&nbsp; 01101000 01101001 00100000 01100010 01101001 01101100 01101100 Web
+            Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
+        </b>
+
+    </div>
+
+    <div id="end">
+        <b>
+            Starbulls Wings and Coffee&nbsp; |&nbsp; 01010110
+            01100101 01101110 01101101 01101111 00100000 00110101 00100000
+            01100010 01110101 01100011 01101011 01110011
+            00001010 Web Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
+        </b>
+    </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Starbulls</title>
+    <link rel="stylesheet" type="text/css" href="css\willStarbull.CSS">
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="images/starbullslogo.png" alt="logo" />
+        </div>
+
+        <h1>STARBULLS
+            <div class="topnav">
+                <a href="menu.php">Menu</a>
+                <a href="specialMenu.php">Specials Of The Week</a>
+                <a href="about.php">About Us</a>
+                <a href="apply.php">Apply Now</a>
+            </div>
+        </h1>
+    </div>
+    </div>
+
+    <div name="ingredients" class="ingredients" id="ingredients">
+        <div class="ingredients-text">
+            <h2>Only the best of the best here at StarBulls</h2>
+            <p>At StarBulls we strive to provide our customers with the best ingredients. You can be assured that we
+                only purchase high-quality ingredients that are ethically sourced.</p>
+        </div>
+    </div>
+
+    <div id="hours">
+        <h2>Hours of Operation</h2>
+        <table class="table">
+            <tr>
+                <td>Sunday</td>
+                <td>*Closed*</td>
+            </tr>
+            <tr>
+                <td>Monday</td>
+                <td>9AM-7PM</td>
+            </tr>
+            <tr>
+                <td>Tuesday</td>
+                <td>9AM-7PM</td>
+            </tr>
+            <tr>
+                <td>Wednesday</td>
+                <td>9AM-7PM</td>
+            </tr>
+            <tr>
+                <td>Thursday</td>
+                <td>9AM-7PM</td>
+            </tr>
+            <tr>
+                <td>Friday</td>
+                <td>9AM-7PM</td>
+            </tr>
+            <tr>
+                <td>Saturday</td>
+                <td>9AM-9PM</td>
+            </tr>
+        </table>
+        </br></br>
+
+        <div class="row">
+            <div class="column">
+                <div id="connect">
+                    <img src="images/connect.jpg" />
+                </div>
+            </div>
+            <div class="column">
+                <div class="connectText">
+                    <h2>Connect with us on Social Media!</h2>
+                    <p>We'd love to hear from YOU!</p> </br>
+                    <p>Follow us to find out the latest buzz!</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+                <div id="connect">
+                    <div class="connectTexts">
+                        <h2>Enjoy a Variety of Food!</h2>
+                        <p>We offer a variety of food and drinks to satisfy any and all cravings!</p> </br>
+                        <p>Head over to our menu page!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <img src="images/wingassortment.jpg" />
+
+            </div>
+        </div> </br></br>
+
+
+        <div id="wingplatter">
+        </div>
+    </div>
+    </div> </br>
+
+    <div id="socialMedia">
+        <a href="https://www.instagram.com/starbulls716/" class="btn btn-default" target="_blank">
+            <img src="images\instagram.png" alt="Instagram"></a>
+        <a href="https://www.facebook.com/starbulls.buffalo/" class="btn btn-default" target="_blank">
+            <img src="images\fb.png" alt="facebook"></a>
+        <a href="https://twitter.com/home/" class="btn btn-default" target="_blank">
+            <img src="images\_twitter.png" alt="twitter"></a>
+    </div> </br>
+    <div id="end">
+        <b>
+            Starbulls Wings and Coffee&nbsp; |&nbsp; 01101000 01101001 00100000 01100010 01101001 01101100 01101100 Web
+            Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
+        </b>
+    </div>
+    </footer>
+
+</body>
+
+</html>
