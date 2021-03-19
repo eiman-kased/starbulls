731164cdbb7a0b2765a133b71b9da07e4af97f7b
Gagan Chhabra <gchhabra24@gmail.com>
2021-03-19 12:15:39 -0400

    create a spreate navbar file

---
+++ nav.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>STARBULLS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css\main.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-custom">
+        <div class="container-fluid">
+            <div class="logo">
+                <a href="index.php" class="logo">
+                    <img src="images/starbullslogo.png" alt="logo" />
+                </a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">ABOUT US</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            MENU
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#cafe">Cafe</a></li>
+                            <li><a class="dropdown-item" href="#breakfastSingles">Breakfast</a></li>
+                            <li><a class="dropdown-item" href="#tradWings">Wings</a></li>
+                            <li><a class="dropdown-item" href="#dinner">Dinner+Sides</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="specialMenu.php">WEEKLY SPECIALS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="apply.php">APPLY NOW</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
