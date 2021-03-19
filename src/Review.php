5c9ff2afb148d7ba29f051a34201bb91556df55a
Nick Burdick <burd2790@gmail.com>
2021-03-19 11:43:14 -0400

    connect to db connect and add review class - also included some privately defined variables

---
+++ src/Review.php
@@ -0,0 +1,14 @@
+<? php 
+
+require_once('src/DB/db_connect.php');
+
+class review {
+    private int $Id;
+    //How is score being done? 
+    private $score;
+    private string $comment;
+    private int $createdAt;
+}
+
+
+?>
