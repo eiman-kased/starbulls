970b442400d31a370bd589c8abc25466bd6ef68b
Eiman Kased <eiman.kased@gmail.com>
2021-03-18 23:01:29 -0400

    fixed the backslash to a forwardslash on css file, fixed all break tags, no longer have to click twice on accordions to expand

--- specialMenu.php
+++ specialMenu.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Specials</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
-	<link rel="stylesheet" type="text/css" href="css\specials.css" />
+	<link rel="stylesheet" type="text/css" href="css/specials.css" />
 </head>
 
 <body>
@@ -27,16 +27,16 @@
 
 	<div class="accordion accordion-flush" id="accordionSpecials">
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="monday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+			<h2 class="accordion-header" id="monday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
 					Milk Chocolate Mondays
 				</button>
 			</h2>
-			<div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="monday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="monday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
 					<p>Come and enjoy hot milk chocolate for a party of 4 for only the price of 3.
 						We'll serve a giant bowl of chocolate and only two spoons so sharing is caring!
-						Milk Chocolate Mondays are a good way to get closer with family and friends. </br>Don't
+						Milk Chocolate Mondays are a good way to get closer with family and friends. <br />Don't
 						have friends?? Have no fear, matchmaking is here. Come to the counter and ask to
 						sign up for a matchmaking table where we set you up with people also looking
 						for new friends or family.... ($50/per table of 4)</p>
@@ -44,53 +44,53 @@
 			</div>
 		</div>
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="tuesday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+			<h2 class="accordion-header" id="tuesday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
 					Tingle Tuesdays
 				</button>
 			</h2>
-			<div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="tuesday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="tuesday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
 					<p>Have you had a long day of web development? Well come to StarBulls on Tuesdays
 						and try our buffalo coffee and buffalo wings combo. I guarantee you will wake up so fast to code
-						a whole project.</br> Bring your computer and pitch ideas to us and if we like it then
+						a whole project.<br /> Bring your computer and pitch ideas to us and if we like it then
 						we will give you one free half eating wing! If we really like the idea we'll throw
 						in an extra bone in there. ($20/combo)</p>
 				</div>
 			</div>
 		</div>
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="wednesday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			<h2 class="accordion-header" id="wednesday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
 					Wings n Wednesdays
 				</button>
 			</h2>
-			<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="wednesday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="wednesday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
 					<p>Tis' the middle of an hardworking week, come to StarBulls and let us get you a great discount
-						on wings. For $100 get yourself an all you can wing buffet for 5 people!!</br> We're talking about
+						on wings. For $100 get yourself an all you can wing buffet for 5 people!!<br /> We're talking about
 						any flavor
 						you can ask for; buffalo, BBQ, honey mustard, garlic, original, blueberry, watermelon, and many
 						more.
-						There's no other restaurant that does it the same way we do.</br> Now come and enjoy an authentic
+						There's no other restaurant that does it the same way we do.<br /> Now come and enjoy an authentic
 						experience here
 						in Buffalo. We will not dissapoint. ($100/per table of 4)</p>
 				</div>
 			</div>
 		</div>
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="thursday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+			<h2 class="accordion-header" id="thursday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
 					Tasty Thursdays
 				</button>
 			</h2>
-			<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="thursday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="thursday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
 					<p>On Thursdays, we completely shut down our kitchen and open up our giant drinks bar. There will be
 						discounted
 						beverages all day long. Even though there aren't going to be any of our food, come and sit down with
 						your friends
-						and meet new people as well.</br> Use this day as a day to recharge yourself with a nice shot of
+						and meet new people as well.<br /> Use this day as a day to recharge yourself with a nice shot of
 						siracha. Spicy not your
 						style? Try our buy one get one of our BBQ beer pints. Live life by the edge and come to StarBulls on
 						Tasty Thursdays.
@@ -99,12 +99,12 @@
 			</div>
 		</div>
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="friday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+			<h2 class="accordion-header" id="friday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
 					Flying Friday
 				</button>
 			</h2>
-			<div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="friday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="friday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
 					<p>If you come to StarBulls on a Friday, prepare to be taken on a ride. We completely transform the
 						restaurant
@@ -121,56 +121,57 @@
 			</div>
 		</div>
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="saturday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+			<h2 class="accordion-header" id="saturday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
 					Seasoned Saturdays
 				</button>
 			</h2>
-			<div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="saturday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="saturday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
 					<p>Stop by on Seasoned Saturdays to try our special homemade dry rub wings seasoning. The seasoning is
 						a secret
 						recipe we keep in a glass bottle where no one will ever think to look. We are proud to be serving
 						this kind
-						of sauce to our supporters on Saturdays.</br> It's one of those sauces that you'd wanna order a
+						of sauce to our supporters on Saturdays.<br /> It's one of those sauces that you'd wanna order a
 						seperate gallon
 						of dry sauce just for your 6 pieces of wings. Along with the wings we serve the most seasoned fries
 						you
-						have ever eaten. </br>We are not done yet, adding onto the seasoned family, try our seasoned beer as
+						have ever eaten. <br />We are not done yet, adding onto the seasoned family, try our seasoned beer as
 						well.
 						($20.95/per 12pc combo) <br /> *Drink responsibly* </p>
 				</div>
 			</div>
 		</div>
 		<div class="accordion-item">
-			<h2 class="accordion-header sticky-top" id="sunday">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-					Sunday *CLOSED*
+			<h2 class="accordion-header" id="sunday">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+					Sleepy Sunday
 				</button>
 			</h2>
-			<div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="sunday" data-bs-parent="#accordianSpecials">
+			<div id="flush-collapseSeven" class="accordion-collapse collapse" aria-labelledby="sunday" data-bs-parent="#accordionSpecials">
 				<div class="accordion-body">
-					<p>Sorry for the inconvenience.</p>
+					<p>*CLOSED* Sorry for the inconvenience.</p>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<div id="socialMedia">
-			<a href="https://www.instagram.com/starbulls716/" class="btn btn-default" target="_blank">
-				<img src="images\instagram.png" alt="Instagram"></a>
-			<a href="https://www.facebook.com/starbulls.buffalo/" class="btn btn-default" target="_blank">
-				<img src="images\fb.png" alt="facebook"></a>
-			<a href="https://twitter.com/home/" class="btn btn-default" target="_blank">
-				<img src="images\_twitter.png" alt="twitter"></a>
-		</div>
-		<div id="end">
-			<b>
-				Starbulls Wings and Coffee&nbsp; |&nbsp; 01101000 01101001 00100000 01100010 01101001 01101100 01101100
-				Web Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
-			</b>
-		</div>
+	<div id="socialMedia">
+		<a href="https://www.instagram.com/starbulls716/" class="btn btn-default" target="_blank">
+			<img src="images\instagram.png" alt="Instagram"></a>
+		<a href="https://www.facebook.com/starbulls.buffalo/" class="btn btn-default" target="_blank">
+			<img src="images\fb.png" alt="facebook"></a>
+		<a href="https://twitter.com/home/" class="btn btn-default" target="_blank">
+			<img src="images\_twitter.png" alt="twitter"></a>
+	</div>
+	<div id="end">
+		<b>
+			Starbulls Wings and Coffee&nbsp; |&nbsp; 01101000 01101001 00100000 01100010 01101001 01101100 01101100
+			Web Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
+		</b>
+	</div>
 
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
 </html>
