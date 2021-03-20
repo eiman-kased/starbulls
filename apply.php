307598c877691cb20eb837ba441120c210a9a469
margeschrec <margeschrec@gmail.com>
2021-03-20 13:46:36 -0400

    resolved commented out code on main.css, fixed some odd formating for socialMedia Div

--- apply.php
+++ apply.php
@@ -292,11 +292,14 @@
 	</div>
 	<div id="socialMedia">
 		<a href="http://www.instagram.com/" class="btn btn-default" target="_blank">
-			<img src="images\instaIcon.jpg" alt="Instagram"></a>
+			<img src="images\instaIcon.jpg" alt="Instagram">
+		</a>
 		<a href="http://www.facebook.com/" class="btn btn-default" target="_blank">
-			<img src="images\faceIcon.jpg" alt="facebook"></a>
+			<img src="images\faceIcon.jpg" alt="facebook">
+		</a>
 		<a href="http://www.twitter.com/" class="btn btn-default" target="_blank">
-			<img src="images\twitterIcon.jpg" alt="twitter"></a>
+			<img src="images\twitterIcon.jpg" alt="twitter">
+		</a>
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
