f1f45055c302eb51ad64aedcb0345df0fcdc7a5d
margeschrec <margeschrec@gmail.com>
2021-03-20 11:49:28 -0400

    moved footer and its css to footerDraft folder

---
+++ footerDraft/footer.php
@@ -0,0 +1,56 @@
+<footer class="container-fluid">
+    <div class="footer row">
+        <div class="col-sm-12 col-lg-4">
+            <div id="socialMedia">
+                <a href="http://www.instagram.com/" class="btn btn-default" target="_blank">
+                    <img src="images\instaIcon.jpg" alt="Instagram"></a>
+                <a href="http://www.facebook.com/" class="btn btn-default" target="_blank">
+                    <img src="images\faceIcon.jpg" alt="facebook"></a>
+                <a href="http://www.twitter.com/" class="btn btn-default" target="_blank">
+                    <img src="images\twitterIcon.jpg" alt="twitter"></a>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+            <a href="#" target="_blank">
+                <p>#
+                </p>
+            </a>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+            <a href="#" target="_blank">
+                <p>#
+                </p>
+            </a>
+        </div>
+    </div>
+</footer>
+
+/* Footer start*/
+
+.footer.row {
+    min-height: 100px;
+    position: relative;
+    background-color: green;
+    color: green;
+    text-align: center;
+}
+
+.footer h2 {
+    padding-top: 1%;
+    text-transform: uppercase;
+    color: white;
+}
+
+.footer p {
+    color: white;
+}
+
+.container-fluid {
+    height: fit-content;
+    background-color: green;
+}
+
+.footer a {
+    color: green;
+    text-decoration: none;
+}
