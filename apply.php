b08b996f8b92c7e374de4c36f87dc17d6a0d01e9
Gagan Chhabra <gchhabra24@gmail.com>
2021-03-20 15:08:47 -0400

    apply.php page was missing the footer.php file

--- apply.php
+++ apply.php
@@ -86,7 +86,7 @@ ini_set('display_errors', 1);
 			</div></br>
 		</div>
 	</div></br>
-	<?php include 'forms/user.form.php'; ?>
+	<?php include 'footer.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
