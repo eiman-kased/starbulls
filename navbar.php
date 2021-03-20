1063ecb5aece6309f58eeb8daf469b66de5ac3cf
Gagan Chhabra <gchhabra24@gmail.com>
2021-03-19 20:47:10 -0400

    removed doctype html from navbar.php, added a missing closing div tah on line 35

--- navbar.php
+++ navbar.php
@@ -1,48 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>STARBULLS</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-custom">
-        <div class="container-fluid">
-            <div class="logo">
-                <a href="index.php" class="logo">
-                    <img src="images/starbullslogo.png" alt="logo" />
-                </a>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">ABOUT US</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="menu.php" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            MENU
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#cafe">Cafe</a></li>
-                            <li><a class="dropdown-item" href="#breakfastSingles">Breakfast</a></li>
-                            <li><a class="dropdown-item" href="#tradWings">Wings</a></li>
-                            <li><a class="dropdown-item" href="#dinner">Dinner+Sides</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="specialMenu.php">WEEKLY SPECIALS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="apply.php">APPLY NOW</a>
-                    </li>
-                </ul>
-            </div>
-    </nav>
-</body>
-
-</html>
+	<nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-custom">
+		<div class="container-fluid">
+			<div class="logo">
+				<a href="index.php" class="logo">
+					<img src="images/starbullslogo.png" alt="logo" />
+				</a>
+			</div>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="about.php">ABOUT US</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="menu.php" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							MENU
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<li><a class="dropdown-item" href="#cafe">Cafe</a></li>
+							<li><a class="dropdown-item" href="#breakfastSingles">Breakfast</a></li>
+							<li><a class="dropdown-item" href="#tradWings">Wings</a></li>
+							<li><a class="dropdown-item" href="#dinner">Dinner+Sides</a></li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="specialMenu.php">WEEKLY SPECIALS</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="apply.php">APPLY NOW</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
