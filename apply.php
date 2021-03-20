a6c4cee8771c445929a293061b44856b5705f7a3
Gagan Chhabra <gchhabra24@gmail.com>
2021-03-20 12:16:19 -0400

    created footer.php file, added footer.php on all pages

--- apply.php
+++ apply.php
@@ -85,15 +85,8 @@ ini_set('display_errors', 1);
 				<p>John L: "StarBulls offers a great astmosphere; everyone is very relaxed!"</p>
 			</div></br>
 		</div>
-	</div>
-	</br>
-	<?php include 'forms/user.form.php';?>
-	<div id="end">
-		<b>
-			Starbulls Wings and Coffee&nbsp; |&nbsp; 01101000 01101001 00100000 01100010 01101001 01101100 01101100 Web Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
-		</b>
-
-	</div>
+	</div></br>
+	<?php include 'forms/user.form.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
