bb41024019559db11a5394bb9d622c3a1940312a
rbaia28 <baia5230@gmail.com>
2021-03-22 16:00:47 -0400

    25 removed social stylesheet and links in pages and added social css to main

--- apply.php
+++ apply.php
@@ -10,7 +10,6 @@ ini_set('display_errors', 1);
 	<title>Apply</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" href="css/socialIcon.css" />
 </head>
 
 <body>
