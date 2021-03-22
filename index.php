fde0a2e68dcacdae291397d03a85f834abb2bee7
Nick Burdick <burd2790@gmail.com>
2021-03-22 09:03:23 -0400

    accepted current changes for index page

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>STARBULLS</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css\main.css">
-    <link rel="stylesheet" href="css/socialIcon.css" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>STARBULLS</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css\main.css">
+	<link rel="stylesheet" href="css/socialIcon.css" />
 </head>
 
 <body>
-    <?php include 'navbar.php'; ?>
-    <div name="ingredients" class="ingredients" id="ingredients">
-        <div class="ingredients-text">
-            <h2>Only the best of the best here at StarBulls</h2>
-            <p>At StarBulls we strive to provide our customers with the best ingredients. You can be assured that we
-                only purchase high-quality ingredients that are ethically sourced.</p>
+    <div class="header">
+        <div class="logo">
+            <a href="index.php" class="logo">
+                <img src="/imgs/starbullslogo.png" alt="logo" />
+            </a>
         </div>
-    </div>
+
+
     <div id="hours">
         <h2>Hours of Operation</h2>
         <table class="table">
@@ -56,7 +56,7 @@
         <div class="row">
             <div class="column">
                 <div id="connect">
-                    <img src="images/connect.jpg" />
+                    <img src="imgs/contactUs.jpeg" />
                 </div>
             </div>
             <div class="column">
@@ -79,85 +79,30 @@
                 </div>
             </div>
             <div class="column">
-                <img src="images/wingassortment.jpg" />
-
-                <div id="hours">
-                    <h2>Hours of Operation</h2>
-                    <table class="table">
-                        <tr>
-                            <td>Sunday</td>
-                            <td>*Closed*</td>
-                        </tr>
-                        <tr>
-                            <td>Monday</td>
-                            <td>9AM-7PM</td>
-                        </tr>
-                        <tr>
-                            <td>Tuesday</td>
-                            <td>9AM-7PM</td>
-                        </tr>
-                        <tr>
-                            <td>Wednesday</td>
-                            <td>9AM-7PM</td>
-                        </tr>
-                        <tr>
-                            <td>Thursday</td>
-                            <td>9AM-7PM</td>
-                        </tr>
-                        <tr>
-                            <td>Friday</td>
-                            <td>9AM-7PM</td>
-                        </tr>
-                        <tr>
-                            <td>Saturday</td>
-                            <td>9AM-9PM</td>
-                        </tr>
-                    </table>
-                    </br></br>
-
-                    <div class="row">
-                        <div class="column">
-                            <div id="connect">
-                                <img src="imgs/contactUs.jpeg" />
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div class="connectText">
-                                <h2>Connect with us on Social Media!</h2>
-                                <p>We'd love to hear from YOU!</p> </br>
-                                <p>Follow us to find out the latest buzz!</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="column">
-                            <div id="connect">
-                                <div class="connectTexts">
-                                    <h2>Enjoy a Variety of Food!</h2>
-                                    <p>We offer a variety of food and drinks to satisfy any and all cravings!</p> </br>
-                                    <p>Head over to our menu page!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <img src="/imgs/chickenWings.jpg" />
-                        </div>
-                    </div> </br></br>
+                <img src="/imgs/chickenWings.jpg" />
+            </div>
+        </div> </br></br>
 
 
-
-                </div>
-            </div> </br>
-
-
-
-        </div>
-    </div> </br></br>
 
     </div>
     </div> </br>
-    <?php include 'footer.php'; ?>
+
+    <div id="socialMedia">
+        <a href="https://www.instagram.com/starbulls716/" class="btn btn-default" target="_blank">
+            <img src="imgs\instagram.png" alt="Instagram"></a>
+        <a href="https://www.facebook.com/starbulls.buffalo/" class="btn btn-default" target="_blank">
+            <img src="imgs\fb.png" alt="facebook"></a>
+        <a href="https://twitter.com/home/" class="btn btn-default" target="_blank">
+            <img src="imgs\_twitter.png" alt="twitter"></a>
+    </div> </br>
+    <div id="end">
+        <b>
+            Starbulls Wings and Coffee&nbsp; |&nbsp; 01101000 01101001 00100000 01100010 01101001 01101100 01101100 Web
+            Development Street&nbsp; |&nbsp; Buffalo , NY 14220&nbsp; |&nbsp;
+        </b>
+    </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
