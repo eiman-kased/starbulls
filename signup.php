a800aeaafeef216e993e216d53b971a56bf6ac6c
margeschrec <margeschrec@gmail.com>
2021-03-23 10:13:18 -0400

    added footer to signup.php

--- signup.php
+++ signup.php
@@ -12,6 +12,7 @@
 	<img src="https://static01.nyt.com/images/2016/02/09/dining/09COOKING_CHICKENWINGS2/09COOKING_CHICKENWINGS2-superJumbo.jpg" id=bg alt="wings">
 	<?php include 'navbar.php'; ?>
 	<?php include 'forms/user.form.php'; ?>
+	<?php include 'footer.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
