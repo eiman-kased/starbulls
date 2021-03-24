75e33c4acc373877b4a934a3d10fbf3f50f6ce9b
will_saulon <willfrombuffalo13@gmail.com>
2021-03-24 18:51:06 -0500

    changed css, capability of reading a file

--- js/indicator.js
+++ js/indicator.js
@@ -1,29 +1,29 @@
 function indicate() {
 	$(document).ready(function () {
-		var currentPage = window.location.pathname;
-
+		var currentPage = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
+		//var thePage = currentPage.substring(currentPage.lastIndexOf('/') + 1);
 		switch (currentPage) {
-			case "/index.php":
+				case "index.php":
 				console.log("click action");
 				$(".nav-item.nav-active").removeClass("nav-active");
 				$('a[href="index.php"').parent().addClass("nav-active");
 				break;
-			case "/about.php":
+			case "about.php":
 				console.log("click action");
 				$(".nav-item.nav-active").removeClass("nav-active");
 				$('a[href="about.php"').parent().addClass("nav-active");
 				break;
-			case "/menu.php":
+			case "menu.php":
 				console.log("click action");
 				$(".nav-item.nav-active").removeClass("nav-active");
 				$('a[href="menu.php"').parent().addClass("nav-active");
 				break;
-			case "/specialMenu.php":
+			case "specialMenu.php":
 				console.log("click action");
 				$(".nav-item.nav-active").removeClass("nav-active");
 				$('a[href="specialMenu.php"').parent().addClass("nav-active");
 				break;
-			case "/apply.php":
+			case "apply.php":
 				console.log("click action");
 				$(".nav-item.nav-active").removeClass("nav-active");
 				$('a[href="apply.php"').parent().addClass("nav-active");
