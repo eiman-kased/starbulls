8c3771685e6d6d120e957b4ad830907c978f4f75
Eiman Kased <eiman.kased@gmail.com>
2021-03-24 23:22:22 -0400

    for #77 refactored db_connect.php into Database class

--- src/User.php
+++ src/User.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'src/DB/db_connect.php';
+require_once 'src/DB/Database.php';
 
 class User
 {
@@ -12,7 +12,7 @@ class User
 	private string $phoneNumber;
 	private bool $isPreferred;
 
-	private static mysqli $db;
+	private Database $db;
 
 	public function __construct(string $firstName, string $lastName, string $email, string $password, string $phoneNumber, bool $isPreferred = false)
 	{
@@ -27,7 +27,8 @@ class User
 	public function saveToDB(): User
 	{
 		// make/get db connection
-		self::$db = self::$db ?? dbConn();
+		$this->db = $this->db ?? new Database();
+		$dbConnection = $this->db->getConnection();
 		// check to make sure this user doesn't already exist, based on email
 		if ($foundUser = self::findUserByEmail($this->email)) {
 			return $foundUser;
@@ -37,27 +38,28 @@ class User
 		$sql = "INSERT INTO `user` (firstName, lastName, email, password, phoneNumber, isPreferred)
 		VALUES ('$this->firstName', '$this->lastName', '$this->email', '$this->password', '$this->phoneNumber', '$preferred')";
 		//echo $sql;
-		$insertSuccess = self::$db->query($sql);
+		$insertSuccess = $dbConnection->query($sql);
 		// Check for errors in the insert process
 		if (!$insertSuccess) {
 			// there was an error
 			// TODO log said error rather than just display
 			// echo 'Debug SQL Statement: ' . $sql . '<br/>';
-			echo 'Error inserting user:' . self::$db->error;
+			echo 'Error inserting user:' . $dbConnection->error;
 			return false;
 		}
 		// everything went well
-		$this->id = self::$db->insert_id;
-		self::$db->close();
+		$this->id = $dbConnection->insert_id;
+		$dbConnection->close();
 		return $this;
 	}
 
 	public static function findUserByEmail($email)
 	{
-		self::$db = self::$db ?? dbConn();
+		$db = new Database();
+		$dbConnection = $db->getConnection();
 		$sql = "SELECT * FROM `user` WHERE email='$email'";
 		// echo $sql;
-		$result = self::$db->query($sql);
+		$result = $dbConnection->query($sql);
 		if ($result->num_rows < 1) {
 			return false;
 		}
@@ -71,7 +73,7 @@ class User
 		// echo '<pre>';
 		// var_dump($tmp);
 		// echo '</pre>';
-		self::$db->close();
+		$dbConnection->close();
 		$retUser =  new User($tmp->firstName, $tmp->lastName, $tmp->email, $tmp->password, $tmp->phoneNumber, $tmp->isPreferred);
 		$retUser->setId($tmp->id);
 		return $retUser;
