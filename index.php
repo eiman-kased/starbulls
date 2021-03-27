0c83d4c8c96bd1f393f41a532719c4dab719c829
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-27 14:23:10 -0400

    updated merge

--- index.php
+++ index.php
@@ -149,14 +149,9 @@ if (session_status() === PHP_SESSION_NONE) {
 		</div>
 		<div class="column">
 			<img src="imgs/chickenWings.jpg" />
-<<<<<<< HEAD
-		</div>-->
-	</div>
-=======
 		</div>
 	</div> </br></br>
 	<?php include 'forms/review.form.php'; ?>
->>>>>>> master
 	<?php include 'footer.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
