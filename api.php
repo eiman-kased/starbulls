127557d634825a28f579d69360cabf7d1b39010c
margeschrec <margeschrec@gmail.com>
2021-04-01 19:49:47 -0400

    wrote get User id api

--- api.php
+++ api.php
@@ -1,11 +1,15 @@
 <?php
+ini_set('display_errors', 1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/vendor/autoload.php';
-echo 'hello world';
+
+require __DIR__ .'/vendor/autoload.php';
+require __DIR__ .'/src/User.php';
+
+// var_dump($_SERVER);
 /**
  * Instantiate App
  *
@@ -25,7 +29,7 @@ $app->addRoutingMiddleware();
  * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
  * @param bool $logErrorDetails -> Display error details in error log
  * which can be replaced by a callable of your choice.
- 
+
  * Note: This middleware should be added last. It will not handle any exceptions/errors
  * for middleware added after it.
  */
@@ -33,9 +37,18 @@ $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 // Define app routes
 $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+	$name = $args['name'];
+	$response->getBody()->write("Hello, $name");
+	return $response;
+});
+
+// lists a users info including their reviews //
+$app->get('/user/{id}', function (Request $request, Response $response, array $args) {
+	$id = $args['id'];
+	$user = \User::findUserById($id);
+	//returning users info
+	$response->getBody()->write(json_encode($user->jsonSerialize()));
+	return $response;
 });
 
 // Run app
