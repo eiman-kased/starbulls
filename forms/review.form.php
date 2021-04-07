8d8592fbe4254bb6013da77860730d3a4624162e
Bill Caffery <billythakidd04@gmail.com>
2021-04-07 19:17:43 -0400

    fixed bug on review form

--- forms/review.form.php
+++ forms/review.form.php
@@ -45,12 +45,12 @@ if (isset($_POST['reviewSubmit'])) {
 		$error['userEmail'] = 'Please Enter you Email';
 	}
 
-	// creating a new review object
-	$review = new Review(null, $score, $comment, null);
-
+	
 	// seeing if the user already exists based on email so we can get the userId based on review
 	$user = \User::findUserByEmail($userEmail);
-
+	
+	// creating a new review object
+	$review = new Review($score, $comment, $user->id);
 	// check to see if the user exists
 	if (!$user) {
 		//if the user doesn't exist, we need to try to create a user
