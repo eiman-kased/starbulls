acecec736813775ee9de26f0e331c31711b43538
Bill Caffery <billythakidd04@gmail.com>
2021-04-10 18:10:48 -0400

    for #110, this is almost finished, we need to get the usthe user id before submitting a review and its done

--- js/reviewApi.js
+++ js/reviewApi.js
@@ -30,27 +30,20 @@ function getUserByEmail(userEmail) {
 		"url": "/user/" + userEmail,
 		"method": "GET"
 	}
+	var userID;
 	// check user route by email to see if the user exists
 	$.ajax(getUserSettings)
 		.done(function (response) { // successful response
 			console.log("success", response);
-			var userID = response.id;
-			console.log("checking response id", response.id);
-			var reviewObj = {
-				'score': $('#reviewScore').val(),
-				'comment': $('#comment').val(),
-				'userID': userID
-			};
-			console.log(reviewObj);
-			// submit to new review using this id
-			createNewReview(reviewObj);
-
-		}).fail(function (response) { // error respons
+			userID = response.id;
+			console.log("checking response id", userID);
+			return userID;
+		}).fail(function (response) { // error response
 			console.log("failure", response);
 			// check response code
-			// if 404
-			// prevent default
-			// show new user form
+			if (response.status == 404) {
+				return false;
+			}
 		});
 }
 
@@ -70,18 +63,78 @@ function createNewReview(reviewObj) {
 			// thank user for adding a review
 		}).fail(function (response) { // error response
 			console.log("failure", response);
-			// update form to reflect reson for failure
+			// update form to reflect reason for failure
 		});
 }
+
+async function createNewUser() {
+	var userObj = {
+		"first_name": $('[name="firstName"]').val(),
+		"last_name": $('[name="lastName"]').val(),
+		"email": $('[name="userEmail"]').val(),
+		"password": $('[name="password"]').val(),
+		"phone": $('[name="tel"]').val()
+	};
+
+	console.log('user object', userObj);
+
+	userSettings = {
+		'url': '/user/new',
+		'method': 'POST',
+		'data': JSON.stringify(userObj),
+		'async': false
+	};
+
+	console.log('user settings', userSettings);
+
+	$.ajax(userSettings)
+		.done(function (response) {
+			alert("Thank you for the review " + response.first_name + "!!");
+			console.log('response', response);
+			console.log('response.id', response.id);
+			return response.id;
+		})
+		.fail(function (response) {
+			alert("broke it");
+			console.log(response);
+		})
+};
 
 $(document).ready(function () {
 	// get all review and display
 	getAllReviews();
 
 	$("#reviewForm").submit(function (e) {
+		reviewObj = {
+			'score': $('#reviewScore').val(),
+			'comment': $('#comment').val(),
+		};
+
 		e.preventDefault();
 		// get email value
-		getUserByEmail($("#userEmail").val());
+		var userID = getUserByEmail($("#userEmail").val());
+		if (userID !== undefined && userID !== false) {
+			reviewObj.userId = userID;
+			console.log("review object:", reviewObj);
+			createNewReview(reviewObj);
+		} else {
+			// show new user form
+			$("#IndexReviewForm").hide();
+			$("#IndexUserForm").show();
+			$("#IndexUserForm #email").val($("#userEmail").val());
+
+			$("#userForm").submit(function (e) {
+				e.preventDefault();
+				createNewUser().then(function (id) {
+					console.log('id:', id)
+					if (id !== undefined && id !== false) {
+						reviewObj.id = id
+						createNewReview(reviewObj);
+					} else {
+						alert('bad user id');
+					}
+				});
+			});
+		}
 	});
 });
-
