5eab2489d766caab2c69d82e52ae7f57f631cd64
Nick Burdick <burd2790@gmail.com>
2021-04-20 15:16:05 -0400

    added function preg_replace to create output format for phone number for #113

--- api.php
+++ api.php
@@ -162,7 +162,8 @@ $app->get('/user/id/{id}', function (Request $request, Response $response, array
 $app->post('/user/new', function (Request $request, Response $response, array $args) {
 	// get request body
 	$body = json_decode($request->getBody());
-	/*Accepted patterns for phone-number
+	
+	/* Check phone number. Accepted patterns for phone-number
 	###-###-####
 	(###)###-####
 	(###) ###-####
@@ -170,7 +171,7 @@ $app->post('/user/new', function (Request $request, Response $response, array $a
 	### ### ####
 	*/
 
-	//string pattern set assigned to variable
+	//string pattern assigned to variable
 	$numberRegEx = '\(?(\d{3})[\)\s-]*(\d{3})[\s\-]?(\d{4})';
 	//Check if pattern matches
 	if (!preg_match($numberRegEx, $body->phone)) {
@@ -185,13 +186,14 @@ $app->post('/user/new', function (Request $request, Response $response, array $a
 			->withStatus(400);
 	}
 	
-	//set string patter to userPhone variable
+	//set string pattern to userPhone variable
 	$userPhone = '\(?(\d{3})[\)\s-]*(\d{3})[\s\-]?(\d{4})';
 	//replace  pattern  with output variables
-	preg_replace($userPhone, ($1)$2$3);
+	function preg_replace($userPhone, "$1$2$3", $body->phone);
+	//output (###) ###-####
 	
 	// create user from request values
-	$user = new User($body->first_name, $body->last_name, $body->email, $body->password, $userPhone, $body->preferred ?? false);
+	$user = new User($body->first_name, $body->last_name, $body->email, $body->password, $body->phone, $body->preferred ?? false);
 	// save the user
 	$user->saveToDB();
 	// set encoded user as response body
