f25c3bfac4d417e8e56ff7d24c184c5c9080416f
margeschrec <margeschrec@gmail.com>
2021-04-21 11:50:06 -0400

    re-added php backend validation to review.form.php

--- forms/review.form.php
+++ forms/review.form.php
@@ -44,6 +44,43 @@ if (isset($_POST['reviewSubmit'])) {
 		// if userEmail is empty, add error message to array to be displayed later
 		$error['userEmail'] = 'Please Enter you Email';
 	}
+
+	// creating a new review object
+	$review = new Review($score, $comment, null);
+
+	// seeing if the user already exists based on email so we can get the userId based on review
+	$user = \User::findUserByEmail($userEmail);
+
+	// check to see if the user exists
+	if (!$user) {
+		//if the user doesn't exist, we need to try to create a user
+		// call session_start so we can utilize the $_SESSION super global to pass data to the user form
+		if (session_status() === PHP_SESSION_NONE) {
+			session_start();
+		}
+
+		// set review_submit element to the JSON value of review object
+		$_SESSION['review_submit'] = json_encode($review);
+
+		// set review_post element to the JSON value of $_POST object
+		$_SESSION['review_post'] = json_encode($_POST);
+
+		// redirects the user to the signup page
+		$URL = 'signup.php';
+		echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+		echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+	} else {
+		// if user does exist, set the found user's ID as userID property of $review.
+		$review->setUserId($user->getId());
+
+		//save $review object to database
+		$review->saveToDB();
+		if ($review->getUserID()) {
+			echo '<script language="javascript">';
+			echo 'alert("Thank you for your review!")';
+			echo '</script>';
+		}
+	}
 }
 ?>
 <div class="inputbox container">
