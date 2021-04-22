6d8f2e97006fc724bb1748540f5f3f6ea67a3401
margeschrec <margeschrec@gmail.com>
2021-04-21 20:55:32 -0400

    for #110 ready to be tested

--- js/reviewApi.js
+++ js/reviewApi.js
@@ -7,15 +7,29 @@ function getAllReviews() {
 
 	$.ajax(settings)
 		.done(function (response) {
+			$("#reviews-carousel").empty();
+			var first = true;
 			response.map(function (r) {
-				$("#apiReview").append(`
-			<div data-review_id=${r.id}>
-			<p>${r.user.email} : </p>
-			<p>${r.score}</p>
-			<p>${r.comment}</p>
-			</div>
-			`)
-			})
+				var active = (first ? 'active' : '');
+				$("#reviews-carousel").append(
+					`<div class="carousel-item review-item ${active}">
+				<div data-review_id="${r.id}" class="customerReview">
+					<img src="imgs/starbulls_icon.png" alt="starbulls icon" id="reviewIcon">
+					<div id="userInfo">
+						<p id="email" class="reviewEmail">${r.user.email}</p>
+					</div>
+					<blockquote>
+						<p class="comment">
+						${r.comment}
+						</p>
+					</blockquote>
+					<p class="score">Score Rating:
+						 <img src="imgs/scoreCoffeeCup${r.score}.png" class="scoreCup">
+					</p>
+				</div>
+			</div>`);
+				first = false;
+			});
 		})
 		.fail(function (err) {
 			console.error(err)
@@ -74,9 +88,6 @@ function createNewUser(review = null, reviewCallback = null) {
 
 	$.ajax(userSettings)
 		.done(function (response) {
-			alert("Thank you for the review " + response.first_name + "!!");
-			console.log('response', response);
-			console.log('response.id', response.id);
 			if (review !== null) {
 				review.id = response.id;
 				reviewCallback(review.id);
@@ -145,50 +156,8 @@ $(document).ready(function () {
 					});
 				} else {
 					// some other error
-					//FIXME response not message not displaying on error
-					console.log(response);
-					alert('Error: ', response.message);
+					alert('Error: ' + response.responseJSON.message);
 				}
 			});
 	});
 });
-
-$(document).ready(function () {
-	var settings = {
-		"url": window.location + "reviews",
-		"method": "GET",
-		"timeout": 0,
-	};
-
-	$.ajax(settings)
-		.done(function (response) {
-			console.log(response);
-			$("#reviews-carousel").empty();
-			var first = true;
-			response.map(function (r) {
-				var active = (first ? 'active' : '');
-				$("#reviews-carousel").append(
-					`<div class="carousel-item review-item ${active}">
-				<div data-review_id="${r.id}" class="customerReview">
-					<img src="imgs/starbulls_icon.png" alt="starbulls icon" id="reviewIcon">
-					<div id="userInfo">
-						<p id="email" class="reviewEmail">${r.user.email}</p>
-					</div>
-					<blockquote>
-						<p class="comment">
-						${r.comment}
-						</p>
-					</blockquote>
-					<p class="score">Score Rating:
-						 <img src="imgs/scoreCoffeeCup${r.score}.png" class="scoreCup">
-					</p>
-				</div>
-			</div>`);
-				first = false;
-			});
-			console.log(response);
-		})
-		.fail(function (err) {
-			console.error(err)
-		});
-})
