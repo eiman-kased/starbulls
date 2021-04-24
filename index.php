ef70645ff4a16408c0b87c0b55cf90e214875d71
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-04-24 16:12:58 -0400

    Removed extranious closing div

--- index.php
+++ index.php
@@ -202,7 +202,6 @@ if (session_status() === PHP_SESSION_NONE) {
 			<img src="imgs/twitter.svg" alt="twitter icon"><a href="https://twitter.com/login"> Twitter: starbulls716</a><br />
 		</div>
 	</div><br />
-	</div>
 	<!-- Footer with Address -->
 	<?php include 'footer.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
